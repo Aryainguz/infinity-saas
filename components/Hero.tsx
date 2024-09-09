83ab8a2ce0cb67cb3dc886eb333233f637048a79
Sahil <sa20128hil@gmail.com>
2024-09-09 15:28:50 +0530

    Feat : Landing page added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -92,7 +92,7 @@ const Hero = () => {
             <div className="relative hidden md:justify-center md:items-center md:inline-flex group">
               <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
               <Link
-              href={'/dashboard'}
+                href={'/dashboard'}
                 className="relative inline-flex items-center justify-center px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full"
                 role="button"
               >
